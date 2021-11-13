86352e22391eb5d1b491f893593d8943ec669351
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2021-11-13 08:25:20 +0200

    Notifications in Laravel part 3
    3. Using Laravel Listeners to Broadcast Changes on Users’ Session

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Events\UserSessionChanged;
+use App\Listeners\BroadcastUserLoginNotification;
+use App\Listeners\BroadcastUserLogoutNotification;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Auth\Events\Logout;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -17,6 +22,14 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        Login::class => [
+          BroadcastUserLoginNotification::class,
+
+        ],
+
+        Logout::class => [
+            BroadcastUserLogoutNotification::class,
         ],
     ];
 
