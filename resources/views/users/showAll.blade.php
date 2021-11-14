68651bd6f998745a522419da757680404f0baff0
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2021-11-14 14:50:53 +0200

    Creating a Realtime API with Laravel part 2
    Showing the List of User to Manipulate It on Realtime

---
+++ resources/views/users/showAll.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+
+                 <ul id="users">
+
+                 </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+@push('scripts')
+
+<script>
+
+// to get all users from db by js
+window.axios.get('/api/users')
+.then((response) => {
+    const usersElement = document.getElementById('users');
+    let users = response.data;
+    users.forEach((user , index) => {
+     let element = document.createElement('li');
+     element.setAttribute('id' , user.id);
+     element.innerText = user.name;
+
+     usersElement.appendChild(element); //Append <li> to the users
+    });
+});
+
+</script>
+
+@endpush
