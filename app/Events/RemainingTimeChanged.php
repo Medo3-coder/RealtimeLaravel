9c37b8ab09325496e6110d5f68d60450fad6f241
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2021-11-16 13:03:08 +0200

    Realtime Game with Laravel Echo part 3
    Creating a Command to Broadcast Game Events on Realtime

--- app/Events/RemainingTimeChanged.php
+++ app/Events/RemainingTimeChanged.php
@@ -9,6 +9,7 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class RemainingTimeChanged implements ShouldBroadcast
 {
@@ -33,6 +34,7 @@ class RemainingTimeChanged implements ShouldBroadcast
      */
     public function broadcastOn()
     {
+        Log::debug("Time : {$this->time}");
         return new Channel('game');
     }
 }
--- app/Events/WinnerNumberGenerated.php
+++ app/Events/WinnerNumberGenerated.php
@@ -9,6 +9,7 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class WinnerNumberGenerated implements ShouldBroadcast
 {
@@ -34,6 +35,7 @@ class WinnerNumberGenerated implements ShouldBroadcast
      */
     public function broadcastOn()
     {
+        Log::debug("Number : {$this->number}");
         return new Channel('game');
     }
 }
