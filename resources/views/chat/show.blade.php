a158d530e3eea6cbb9b95afa48327834f05959c0
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2021-11-16 23:44:11 +0200

    Chat Room Using Realtime Messages with Laravel Echo part 4
    Broadcasting the Event Created When Sending a Message

--- resources/views/chat/show.blade.php
+++ resources/views/chat/show.blade.php
@@ -103,5 +103,20 @@
 
     </script>
 
+
+    <script>
+        const messageElement = document.getElementById('message');
+        const sendElement = document.getElementById('send');
+
+        sendElement.addEventListener('click' , (e) => {
+            e.preventDefault();
+            window.axios.post('/chat/message' , {
+                message : messageElement.value,
+            });
+            messageElement.value = ''  ;  // to allow user to send message again
+        })
+
+    </script>
+
 @endpush
 
--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,5 @@ Route::view('/game', 'game.show')->name('game.show');
 
 // chat routs
 Route::get('/chat' , [ChatController::class , 'showChat'])->name('chat.show');
+
+Route::post('/chat/message' , [ChatController::class , 'messageRecived'])->name('chat.message');
